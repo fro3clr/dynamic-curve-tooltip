77f6527d8197e552095ae1b2a61a072abcca7662
fro3clr <fr0zclr@gmail.com>
2017-11-04 19:43:30 +0200

    animated buttons and dynamic positions

--- src/Node.js
+++ src/Node.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovered: false
+    };
+  }
+
   componentDidUpdate() {
     const canvas = this.props.canvas,
       { edit, box } = this.refs;
@@ -15,10 +22,14 @@ class Node extends Component {
   }
 
   computeTooltipPosition() {
-    const tooltip = this.refs.edit;
+    const tooltip = this.refs.edit,
+      delimiter = 7,
+      maxDynamicPos = 220,
+      minDynamicPos = 85;
 
     let dynamicPosition = window.innerWidth / 7;
-    dynamicPosition < 85 ? (dynamicPosition = 85) : false;
+    dynamicPosition < minDynamicPos ? (dynamicPosition = minDynamicPos) : false;
+    dynamicPosition > maxDynamicPos ? (dynamicPosition = maxDynamicPos) : false;
 
     switch (this.props.tooltipPosition) {
       case "top-left":
@@ -42,11 +53,27 @@ class Node extends Component {
     }
   }
 
+  handleHover = () => {
+    this.setState({
+      isHovered: !this.state.isHovered
+    });
+  };
+
   render() {
     return (
       <div className={this.props.className}>
         <div ref="box" className="box">
-          <div ref="edit" className={"edit " + this.props.tooltipPosition} />
+          <div ref="edit" className={"edit " + this.props.tooltipPosition}>
+            <div
+              onMouseEnter={this.handleHover}
+              onMouseLeave={this.handleHover}
+              className="icon pen"
+            />
+            <div className={this.state.isHovered ? "icon trash active" : "icon trash"} />
+            <div
+              className={this.state.isHovered ? "icon camera active" : "icon camera"}
+            />
+          </div>
         </div>
       </div>
     );

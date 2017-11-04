93484ac572dd3feb5431d076bc5848c64a2c6277
fro3clr <fr0zclr@gmail.com>
2017-11-04 23:21:21 +0200

    changes in dynamic positions

--- src/Node.js
+++ src/Node.js
@@ -26,37 +26,55 @@ class Node extends Component {
   computeTooltipPosition() {
     const tooltip = this.refs.edit,
       delimiter = 7,
-      maxDynamicPos = 220,
-      minDynamicPos = 105;
+      maxDynamicPosHeight = 220,
+      minDynamicPosHeight = 60,
+      maxDynamicPosWidth = 220,
+      minDynamicPosWidth = 105;
 
-    let dynamicPosition = window.innerWidth / 12;
-    dynamicPosition < minDynamicPos ? (dynamicPosition = minDynamicPos) : false;
-    dynamicPosition > maxDynamicPos ? (dynamicPosition = maxDynamicPos) : false;
+    let dynamicPositionHeight = window.innerWidth / 12;
+    dynamicPositionHeight < minDynamicPosHeight
+      ? (dynamicPositionHeight = minDynamicPosHeight)
+      : false;
+      dynamicPositionHeight > maxDynamicPosHeight
+      ? (dynamicPositionHeight = maxDynamicPosHeight)
+      : false;
+
+    let dynamicPositionWidth = window.innerWidth / 12;
+    dynamicPositionWidth < minDynamicPosWidth
+      ? (dynamicPositionWidth = minDynamicPosWidth)
+      : false;
+      dynamicPositionWidth > maxDynamicPosWidth
+      ? (dynamicPositionWidth = maxDynamicPosWidth)
+      : false;
 
     switch (this.props.tooltipPosition) {
       case "top-left":
-        tooltip.style.right = tooltip.style.bottom = dynamicPosition + "px";
+        tooltip.style.right = dynamicPositionWidth + "px";
+        tooltip.style.bottom = dynamicPositionHeight + "px";
         break;
 
       case "top-right":
-        tooltip.style.left = tooltip.style.bottom = dynamicPosition + "px";
+        tooltip.style.left = dynamicPositionWidth + "px";
+        tooltip.style.bottom = dynamicPositionHeight + "px";
         break;
 
       case "bottom-left":
-        tooltip.style.right = tooltip.style.top = dynamicPosition + "px";
+        tooltip.style.right = dynamicPositionWidth + "px";
+        tooltip.style.top = dynamicPositionHeight + "px";
         break;
 
       case "bottom-right":
-        tooltip.style.left = tooltip.style.top = dynamicPosition + "px";
+        tooltip.style.left = dynamicPositionWidth + "px";
+        tooltip.style.top = dynamicPositionHeight + "px";
         break;
 
       case "top":
-        tooltip.style.bottom = dynamicPosition + "px";
+        tooltip.style.bottom = dynamicPositionHeight + "px";
         tooltip.style.left = 82 + "px";
         break;
 
       case "bottom":
-        tooltip.style.top = dynamicPosition + "px";
+        tooltip.style.top = dynamicPositionHeight + "px";
         tooltip.style.left = 82 + "px";
         break;
 
